2e4cfb4b47f8b212e00e032ff8336a3826969ab5
iushport <chettriaayush90@gmail.com>
2026-08-01 00:29:28 +0545

    2nd commit

---
+++ main.py
@@ -0,0 +1,17 @@
+print("Hello world") #output
+first_name = "Ayush" #variables first_name bhaneko container
+age = 20 #age container in this 20 chai variable 
+
+#to print variable variable
+print("first_name") # wrong 
+print(first_name) #correct way to print variable value
+print(first_name, age)
+
+#changing variable values
+first_name = "Abiral Gautam"
+age = 26
+print(first_name, age)
+
+#we can check type of vaRiable
+print(type(age))
+
